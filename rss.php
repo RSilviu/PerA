04a77db5f2ad2ecbf3906a1796b833e9fa76d405
RSilviu <1silviu.rusu@gmail.com>
2018-06-16 23:26:48 +0300

    add php rss generator script

---
+++ rss.php
@@ -0,0 +1,31 @@
+<?php
+include "authCheck.php";
+$personId = $_GET['id'];
+include 'db/conn.php';
+$rss = '<?xml version="1.0" ?>
+<rss version="2.0">
+<channel>
+    <title>PerA</title>
+    <link>http://localhost:8080/PerA/</link>
+    <description>Personal Web Assistant</description>';
+$select = 'SELECT name, description FROM activities WHERE userId = ?';
+$stmt = $pdo->prepare($select);
+$stmt->execute([$personId]);
+while ($activity = $stmt->fetch()) {
+    $actName = $activity['name'];
+    $actDesc = $activity['description'];
+    $rss .= "
+<item>
+     <title>$actName</title>
+     <link>others.php?id=person_id</link>
+     <description>$actDesc</description>
+</item>";
+}
+$rss .= '
+</channel>
+</rss>';
+
+header('Content-Type: application/rss+xml');
+echo $rss;
+
+
